f92dd3e578bd85b91f1850e28b27a9d055e6040e
jakubsitko <80541768+jakubsitko@users.noreply.github.com>
2022-03-21 21:42:07 +0100

    binary search

---
+++ src/com.company/binarySearch.java
@@ -0,0 +1,30 @@
+package src.com.company;
+
+public class binarySearch {
+
+    public static void main(String[] args) {
+
+        int[] array = {4, 5, 7, 11, 12, 15, 15, 21, 40, 45};
+        int index = searchIndex(array, 11);
+        System.out.println(index);
+    }
+
+    public static int searchIndex(int[] array, int value) {
+        int index = 0;
+        int limit = array.length - 1;
+        while (index <= limit) {
+            int point = (int) Math.ceil((index + limit) / 2);
+            int entry = array[point];
+            if (value > entry) {
+                index = point + 1;
+                continue;
+            }
+            if (value < entry) {
+                limit = point - 1;
+                continue;
+            }
+            return point;//value==entry
+        }
+        return -1;
+    }
+}
